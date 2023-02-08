b9556e24fa104e3b414c4ea15902efe77119402a
NguyenVanHoang3103 <117966982+NguyenVanHoang3103@users.noreply.github.com>
2023-02-08 23:03:12 +0700

    update code

---
+++ App.js
@@ -0,0 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Demo10 from './Demo10';
+import Demo11 from './Demo11';
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Demo10}
+          options={{ title: 'Trang chủ' }} />
+        <Stack.Screen name="Demo11" component={Demo11} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
